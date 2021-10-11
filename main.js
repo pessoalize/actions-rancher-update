1026a64f4edde889f2d1bfdb563d0b203c852784
Nicolas Dezena <ndezena@gmail.com>
2021-10-11 15:02:54 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ async function main() {
   const namespace = core.getInput('namespace', {required: true});
   const deployment = core.getInput('deployment', {required: true});
   const dockerImage = core.getInput('docker_image', {required: true});
+  const showErrorLogs = core.getInput('error_logs', {required: false});
 
   await axios.patch(
     `${rancherUrl}/k8s/clusters/${clusterId}/apis/apps/v1/namespaces/${namespace}/deployments/${deployment}`,
@@ -30,16 +31,31 @@ async function main() {
     },
   );
 
-  await axios.post(
-    `${rancherUrl}/v3/projects/${clusterId}:${projectId}/workloads/deployment:${namespace}:${deployment}?action=redeploy`,
-    {},
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + rancherToken,
+  try {
+      await axios.post(
+      `${rancherUrl}/v3/projects/${clusterId}:${projectId}/workloads/deployment:${namespace}:${deployment}?action=redeploy`,
+      {},
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + rancherToken,
+        },
       },
-    },
-  );
+    );
+  } catch (e) {
+     if(showErrorLogs) console.log(e);
+     console.log("Provavelmente o workload é um por node")
+     await axios.post(
+      `${rancherUrl}/v3/projects/${clusterId}:${projectId}/workloads/daemonset:${namespace}:${deployment}?action=redeploy`,
+      {},
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + rancherToken,
+        },
+      },
+    );
+  }
 }
 
 function handleError(err) {
